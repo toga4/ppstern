06791f7c1013ad58a284f11ef81d585cc5bbc739
toga4 <81744248+toga4@users.noreply.github.com>
2023-11-15 17:47:39 +0900

    avoid cast []byte <> string

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		in := scanner.Text()
-		s, err := ppstern.ParseAndFormat([]byte(in))
+		in := scanner.Bytes()
+		s, err := ppstern.ParseAndFormat(in)
 		if err != nil {
 			panic(err)
 		}
